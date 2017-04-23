b707ebaa385f8ce2aebbef2c47d16e3676a61a12
brandonking7 <kingrome74@gmail.com>
2017-04-23 05:10:59 -0400

    Server running...added client public and server directories

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.listen(4000, function() {
+  console.log('Server now listening sir.')
+});
