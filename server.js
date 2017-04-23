9286429ea73b4f2bef75e8c830643a09a102293d
brandonking7 <kingrome74@gmail.com>
2017-04-23 18:10:38 -0400

    finally got webpack to work added angular

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const logger = require('morgan');
 
 app.use(express.static(__dirname + '/public'));
+
+//Server
 app.listen(4000, function() {
-  console.log('Server now listening sir.')
+  console.log('Server now listening sir.');
 });
