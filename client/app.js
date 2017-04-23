96fc132d9a231fd1ab87904d773cd2897c0ca84e
brandonking7 <kingrome74@gmail.com>
2017-04-23 19:10:24 -0400

    added states to app.js

--- client/app.js
+++ client/app.js
@@ -1,3 +1,29 @@
 const angular = require('angular');
+require('angular-ui-router')
 
-angular.module('mywork', []);
+angular
+  .module('mywork', ['ui.router'])
+  .config(uiRouterSetup);
+
+  uiRouterSetup.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+  function uiRouterSetup($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state('home', {
+      url: '/',
+      template: '<home></home>'
+    })
+    .state('about', {
+      url: '/about',
+      template: '<about></about>'
+    })
+    .state('list', {
+      url: '/list',
+      template: '<list></list>'
+    })
+    .state('project', {
+      url: '/project',
+      template: '<project></project>'
+    });
+    $urlRouterProvider.otherwise('/');
+  }
