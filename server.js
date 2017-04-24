e723c14063841a8b7d297a660dbbc30f024340ff
brandonking7 <kingrome74@gmail.com>
2017-04-24 17:34:16 -0400

    added router and controller

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const logger = require('morgan');
 
+const projectsController = require('./server/controllers/projects.js');
+app.use('/projects', projectsController);
+
 app.use(express.static(__dirname + '/public'));
 
 //Server
--- server/controllers/projects.js
+++ server/controllers/projects.js
@@ -3,16 +3,28 @@ const router = express.Router();
 
 const projects = [
   {
-    name:
-    description:
-    imageUrl:
-  }
+    name: 'Blackjack',
+    description: 'For project #1, I created a simple game of Blackjack. Once players come to the site, they are shown an alert with a tutorial. Then then hit deal to be dealt a set of cards where the goal of the game is to be the first to 21 or have a bigger score than the dealer period.',
+    img: 'https://s3.amazonaws.com/uifaces/faces/twitter/tonypeterson/128.jpg'
+  },
+  {
+    name: 'Cars',
+    description: 'For project #1, I created a simple game of Blackjack. Once players come to the site, they are shown an alert with a tutorial. Then then hit deal to be dealt a set of cards where the goal of the game is to be the first to 21 or have a bigger score than the dealer period.',
+    img: 'https://s3.amazonaws.com/uifaces/faces/twitter/tonypeterson/128.jpg'
+  },
+  {
+    name: 'Cornholer',
+    description: 'For project #1, I created a simple game of Blackjack. Once players come to the site, they are shown an alert with a tutorial. Then then hit deal to be dealt a set of cards where the goal of the game is to be the first to 21 or have a bigger score than the dealer period.',
+    img: 'https://s3.amazonaws.com/uifaces/faces/twitter/tonypeterson/128.jpg'
+  },
+
 ];
 
 router.get('/', function(req, res) {
   res.json({projects: projects});
 });
 
-router.get('/name' function(req, res) {
-
-});
+// router.get('/name' function(req, res) {
+//
+// });
+module.exports = router;
