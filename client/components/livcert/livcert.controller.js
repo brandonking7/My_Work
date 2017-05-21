6b48a03bd8ba34a3af5932c347c56b5c53bffda6
brandonking7 <kingrome74@gmail.com>
2017-05-20 20:23:44 -0400

    updated livcert component and added on click function

--- client/components/livcert/livcert.controller.js
+++ client/components/livcert/livcert.controller.js
@@ -1,5 +1,15 @@
-function LivcertController() {
+LivcertController.$inject = ['$state'];
+
+function LivcertController($state) {
   const vm = this;
+
+  $('button').on('click', function() {
+    $('#card').addClass('animated zoomOut');
+
+    setTimeout(function() {
+      $state.go('portfolio')
+    }, 100);
+  });
 }
 
 module.exports = LivcertController;
