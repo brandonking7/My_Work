bfc968c3ab31eae978993f409a0b6ca1c54cd882
brandonking7 <kingrome74@gmail.com>
2017-05-20 20:06:27 -0400

    updated cars component

--- client/components/cars/cars.controller.js
+++ client/components/cars/cars.controller.js
@@ -1,5 +1,14 @@
-function CarsController() {
+CarsController.$inject = ['$state']
+function CarsController($state) {
   const vm = this;
+
+  $('button').on('click', function() {
+    $('#card').addClass('animated zoomOut')
+
+    setTimeout(function() {
+      $state.go('portfolio');
+    }, 100);
+  });
 }
 
 module.exports = CarsController;
