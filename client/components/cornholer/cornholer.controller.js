ff5419b8e1d18cb50a61a481feee550df6d708c3
brandonking7 <kingrome74@gmail.com>
2017-05-20 20:36:48 -0400

    updated cornholer component and add on click method

--- client/components/cornholer/cornholer.controller.js
+++ client/components/cornholer/cornholer.controller.js
@@ -1,5 +1,15 @@
-function CornholerController() {
+CornholerController.$inject = ['$state'];
+
+function CornholerController($state) {
   const vm = this;
+
+  $('button').on('click', function() {
+    $('#card').addClass('animated zoomOut');
+
+    setTimeout(function() {
+      $state.go('portfolio');
+    }, 100);
+  });
 }
 
 module.exports = CornholerController;
