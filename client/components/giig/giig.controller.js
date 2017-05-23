6ea3c01d09be04f5f60cc5cc1537930087fd332c
brandonking7 <kingrome74@gmail.com>
2017-05-22 22:13:59 -0400

    Heroku url change

--- client/components/giig/giig.controller.js
+++ client/components/giig/giig.controller.js
@@ -1,5 +1,14 @@
-function GiiGController() {
+GiiGController.$inject = ['$state'];
+function GiiGController($state) {
   const vm = this;
+
+  $('button').on('click', function() {
+    $('#card').addClass('animated zoomOut');
+
+    setTimeout(function() {
+      $state.go('portfolio');
+    }, 100);
+  });
 }
 
 module.exports = GiiGController;
--- client/components/vagabond/vagabond.controller.js
+++ client/components/vagabond/vagabond.controller.js
@@ -1,5 +1,14 @@
-function VagabondController() {
+VagabondController.$inject = ['$state'];
+function VagabondController($state) {
   const vm = this;
+
+  $('button').on('click', function() {
+    $('#card').addClass('animated zoomOut');
+
+    setTimeout(function() {
+      $state.go('portfolio');
+    }, 100);
+  });
 }
 
 module.exports = VagabondController;
